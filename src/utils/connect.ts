5a05e903c7f449afc41f8e80026c6d8954f3c50c
k.stopczynska <k.stopczynska@gmail.com>
2023-10-18 15:45:55 +0200

    fixed issue with prisma types

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,12 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-let prisma;
-if (process.env.NODE_ENV === 'production') {
-	prisma = new PrismaClient();
-} else {
-	if (!prisma) {
-		prisma = new PrismaClient();
-	}
-	prisma = prisma;
-}
+
+	const prisma = new PrismaClient();
+
+
 
 export default prisma;
