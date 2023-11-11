9c038001bf009f3ee3a10f0b54405d7fc182aacc
k.stopczynska <k.stopczynska@gmail.com>
2023-11-11 18:12:38 +0100

    added swiper for card list on smaller screens

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import CardList from '@/components/CardList';
+import CardListSwiper from '@/components/CardListSwiper';
 import Hero from '@/components/Hero';
 
 export default function Home() {
+
 	return (
 		<>
-			<Hero/>
-			<CardList />
+			<Hero />
+			{/* <CardList /> */}
+			<CardListSwiper />
 		</>
 	);
 }
---
+++ src/components/CardListSwiper.tsx
@@ -0,0 +1,77 @@
+'use client';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/free-mode';
+import { FreeMode, Pagination } from 'swiper/modules';
+import useSWR from 'swr';
+import Card from '@/UI/Card';
+import { SearchParamsType, CardType } from '@/types';
+
+type UpdatedSearchParamsType = {
+	[K in keyof SearchParamsType]: K extends 'page'
+		? number
+		: SearchParamsType[K];
+};
+
+const fetcher = async (url: string) => {
+	const res = await fetch(url);
+	const data = await res.json();
+	if (!res.ok) {
+		const error = new Error(data.message);
+		throw error;
+	}
+	return data;
+};
+
+const CardList = ({ page, category }: UpdatedSearchParamsType) => {
+	const { data, mutate, isLoading } = useSWR(
+		`http://localhost:3000/api/posts?page=${page}&cat=${category || ''}`,
+		fetcher,
+	);
+
+	return (
+		<section className='overflow-hidden py-10 border-y-2 border-secondary-200'>
+			<h3 className='text-lg md:text-2xl font-bold mb-20'>Posts</h3>
+			<div className='flex items-center justify-center flex-col gap-6 h-[100%] w-full '>
+				<Swiper
+					breakpoints={{
+						340: {
+							slidesPerView: 1,
+							spaceBetween: 15,
+						},
+						700: {
+							slidesPerView: 2,
+							spaceBetween: 15,
+						},
+						1000: {
+							slidesPerView: 3,
+							spaceBetween: 15,
+						},
+					}}
+					freeMode={true}
+					pagination={{
+						clickable: true,
+					}}
+					modules={[FreeMode, Pagination]}
+					className='w-full gap-6 h-[430px]'
+				>
+					{data?.posts?.map((card: CardType) => (
+						<SwiperSlide>
+							<div
+								className='flex flex-col gap-6 relative group w-full
+	h-[420px]'
+								key={card._id}
+							>
+								<Card {...card} key={card._id} />
+							</div>
+						</SwiperSlide>
+					))}
+				</Swiper>
+			</div>
+		</section>
+	);
+};
+
+export default CardList;
