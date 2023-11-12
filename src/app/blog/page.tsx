315a13ccc14d0fc7085e2ef851b9e49fe5bfc209
k.stopczynska <k.stopczynska@gmail.com>
2023-11-12 17:08:16 +0100

    fixed blog page if there is no category chosen

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -13,7 +13,7 @@ export default function BlogPage({
 	return (
 		<div>
 			<h1 className='text-4xl font-bold text-center mt-20 mb-10 capitalize'>
-				{category.title}
+				{category ? category.title : 'Blog'}
 			</h1>
 			<CardListSwiper page={page} category={category} />
 		</div>
