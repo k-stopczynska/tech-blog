0fdd1fe30bd3ebedbd2bff66eb56bbffefe2a5d8
k.stopczynska <k.stopczynska@gmail.com>
2024-02-29 20:21:12 +0100

    changed prisma initialization

--- src/utils/connect.ts
+++ src/utils/connect.ts
@@ -1,5 +1,18 @@
-import { PrismaClient } from '@prisma/client';
+// import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
+
+// export default prisma;
+
+import { PrismaClient } from '@prisma/client';
+let prisma;
+if (process.env.NODE_ENV === 'production') {
+	prisma = new PrismaClient();
+} else {
+	if (!prisma) {
+		prisma = new PrismaClient();
+	}
+	prisma = prisma;
+}
 
 export default prisma;
