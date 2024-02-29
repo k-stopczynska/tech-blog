1be3991742c5f5bb7b271322fc094f625bbc9467
k.stopczynska <k.stopczynska@gmail.com>
2024-02-29 14:09:16 +0100

    restored previous styles for categories links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
 	return (
-		<footer className='flex items-center justify-center h-12'>
+		<footer className='flex items-center justify-center h-12 bottom-0'>
 			@copyright 2023
 		</footer>
 	);
