e45d90569bb620360da7c44578a22ee30108482c
k.stopczynska <k.stopczynska@gmail.com>
2024-02-29 20:28:20 +0100

    removed server components to narrow down problem

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,13 @@ import CardList from '@/components/CardList';
 import Hero from '@/components/Hero';
 import { SearchParamsType } from '@/types';
 
-export default function Home({ searchParams }: { searchParams: SearchParamsType }) {
-		const page = parseInt(searchParams.page) || 1;
-		const { category } = searchParams;
+export default function Home({
+	searchParams,
+}: {
+	searchParams: SearchParamsType;
+}) {
+	const page = parseInt(searchParams.page) || 1;
+	const { category } = searchParams;
 	return (
 		<>
 			<Hero />
