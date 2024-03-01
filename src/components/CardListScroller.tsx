c49a138223f3270c0a9df1463f4787729a84d9fa
k.stopczynska <k.stopczynska@gmail.com>
2024-03-01 20:15:02 +0100

    added space between list elements, changed justifying headings in post, adjusted card trigger height again

--- src/components/CardListScroller.tsx
+++ src/components/CardListScroller.tsx
@@ -39,7 +39,7 @@ const CardListScroller = ({ page, category }: UpdatedSearchParamsType) => {
 				duration: 1,
 				scrollTrigger: {
 					trigger: triggerRef.current,
-					start: '0 top',
+					start: '-360 top',
 					end: '1500 top',
 					scrub: 0.6,
 					pin: true,
