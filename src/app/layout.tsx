f94259d1a424b136e67125dd8c656a8e83ad5209
k.stopczynska <k.stopczynska@gmail.com>
2024-03-07 13:16:03 +0100

    got rid of footer till figuring out the contact section problem

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,7 @@ export default function RootLayout({
 					<Wrapper>
 						<Navbar />
 						<main>{children}</main>
-						<Footer />
+						{/* <Footer /> */}
 					</Wrapper>
 				</body>
 			</AuthProvider>
