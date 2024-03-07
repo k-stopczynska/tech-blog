0e6b783d9dc7e91074add8046a192ed46ff6d2ac
k.stopczynska <k.stopczynska@gmail.com>
2024-03-07 20:29:50 +0100

    made modal reusable

--- src/UI/Modal.tsx
+++ src/UI/Modal.tsx
@@ -6,21 +6,32 @@ import Link from 'next/link';
 const Modal = () => {
 	const searchParams = useSearchParams();
 	const modal = searchParams.get('modal');
+	const variant = searchParams.get('variant');
 	const pathname = usePathname();
+	let title =
+		variant === 'cv'
+			? 'CV'
+			: variant === 'tech'
+			? 'Tech Stack'
+			: 'Projects';
+
+	let content =
+		variant === 'cv'
+			? 'This is CV image component'
+			: variant === 'tech'
+			? 'This is tech stack component'
+			: 'This is projects component';
 	return (
 		<>
 			{modal && (
 				<dialog className='fixed left-0 top-0 w-full h-full bg-black bg-opacity-50 z-50 overflow-auto backdrop-blur flex justify-center items-center'>
 					<div className='bg-white m-auto p-8'>
 						<div className='flex flex-col items-center'>
-							<h3>Modal content</h3>
-							<br />
+							<h3>{title}</h3>
+							<p>{content}</p>
 							<Link href={pathname}>
-								<button
-									type='button'
-									className='bg-red-500 text-white p-2 '
-								>
-									Close Modal
+								<button className='shadow text-center rounded py-3 px-6 mt-4'>
+									close
 								</button>
 							</Link>
 						</div>
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,31 +9,33 @@ const AboutPage = () => {
 			<div className='relative object-cover w-full top-[8rem] left-[50%] translate-x-[-50%] md:top-24'>
 				<img src='/imaginary_room2.jpg' alt='pixel art room image' />
 				<div className='absolute w-full h-full xl:w-[96.5%] top-0 left-0'>
-					<div
-						role='anchor'
-						className='relative w-[20%] h-[15%] top-[23%] left-[13.5%] cursor-pointer shadow-custom transition-all hover:shadow-hover focus:shadow-hover '
-					>
-						<a
+					<div className='relative w-[20%] h-[15%] top-[23%] left-[13.5%] cursor-pointer shadow-custom transition-all hover:shadow-hover focus:shadow-hover '>
+						<Link href={`?modal=true&variant=cv`}>
+							<button className='absolute top-0 bottom-0 left-0 right-0 text-center opacity-0 transition-opacity hover:opacity-100'>
+								click to see CV
+							</button>
+						</Link>
+						{/* <a
 							href='/klaudia_stopczynska.pdf'
 							target='_blank'
 							rel='noopener noreferrer'
 							download
 							className='absolute top-0 bottom-0 left-0 right-0 p-2 text-center  opacity-0 transition-opacity hover:opacity-100'
-						>
-							click to download CV
-						</a>
+						> 
+							
+						</a> */}
 					</div>
 					<div className='relative w-[21%] h-[28%] top-[27%] left-[13%] rounded-[3.5rem] cursor-pointer transition-all shadow-custom hover:shadow-hover focus:shadow-hover'>
-						<a
-							href='/projects'
+						<Link
+							href={`?modal=true&variant=projects`}
 							target='_blank'
 							className='absolute top-0 bottom-0 left-0 right-0 p-6 text-center  opacity-0 transition-opacity hover:opacity-100'
 						>
 							click to see projects page
-						</a>
+						</Link>
 					</div>
 					<div className='relative w-[23%] h-[7%] top-[9%] left-[57%] cursor-pointer transition-all shadow-custom hover:shadow-hover focus:shadow-hover'>
-						<Link href='?modal=true'>
+						<Link href={`?modal=true&variant=tech`}>
 							<button className='absolute top-0 bottom-0 left-0 right-0 text-center opacity-0 transition-opacity hover:opacity-100'>
 								click to see tech-stack
 							</button>
