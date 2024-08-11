17310410843303cc9f5d8abec9e84d9d74620040
k.stopczynska <k.stopczynska@gmail.com>
2024-08-11 17:09:14 +0200

    build hot fix

--- src/components/PersonalMemo.tsx
+++ src/components/PersonalMemo.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const PersonalMemo = () => {
 	return (
-		<article className="">
+		<article className=''>
 			<h2 className='text-6xl font-extrabold pt-12 pb-4'>
 				About the author
 			</h2>
@@ -12,15 +12,15 @@ const PersonalMemo = () => {
 				Evolution Typescript Bootcamp, delving into Typescript, state
 				management, and 2D/3D animations. Afterwords, I completed the
 				CodeFirstGirls NanoDegree Software Stream, mastering Python,
-				Flask, and SQL. At my dayjob I'm mostly working with Java maintaining
-				and developing applications, and delving into observability, CI/CD
-				and security in the world of finance.
+				Flask, and SQL. At my dayjob I&apos;m mostly working with Java
+				maintaining and developing applications, and delving into
+				observability, CI/CD and security in the world of finance.
 			</p>
 			<p className='pb-4'>
 				Author and maintainer of codegroove- VSCode extension for
-				tracking and analyzing coding habits to master future projects'
-				estimations. Currently working on focus music module using
-				youtube API, loads of fun!
+				tracking and analyzing coding habits to master future
+				projects&apos; estimations. Currently working on focus music
+				module using youtube API, loads of fun!
 			</p>
 			<p className='pb-4'>
 				Passionate writer about everything tech (and not only) related.
