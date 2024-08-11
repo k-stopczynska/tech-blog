3f4dbf5a682ba8efc98f6f13059c20897c00f96f
k.stopczynska <k.stopczynska@gmail.com>
2024-08-11 12:47:07 +0200

    refactored to correctly display project on every device

--- src/UI/ProjectCard.tsx
+++ src/UI/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import useWindowDimensions from '@/hooks/useWindowDimensions';
 import StackElem from './StackElem';
 import ProjectLinkElem from './ProjectLinkElem';
 import { Project, ProjectLink } from '@/types';
@@ -11,39 +12,57 @@ const ProjectCard = ({
 	index,
 	hoveredProject,
 }: Project & { index: number; hoveredProject: number | null }) => {
-	return (
-		<div className='relative'>
-			<img
-				src={img}
-				className={`absolute bottom-0 rounded ${
+	const { height, width } = useWindowDimensions();
+
+	let imgClassName =
+		width! >= 1200
+			? `absolute bottom-0 rounded ${
 					index === hoveredProject ? 'img-fade-in' : 'img-fade-out'
-				} ${index === hoveredProject ? 'opacity-100' : 'opacity-0'}`}
-				alt={`${title} image`}
-			/>
-			<div
-				className={`absolute top-0 w-full min-h-[250px] mt-6 rounded border border-light-100 p-4 flex flex-col justify-between gap-4 ${
+			  } ${index === hoveredProject ? 'opacity-100' : 'opacity-0'}`
+			: 'rounded';
+
+	let contentWrapperClassName =
+		width! >= 1200
+			? `absolute top-0 w-full min-h-[250px] mt-6 rounded border border-light-100 p-4 flex flex-col justify-between gap-4 ${
 					index === hoveredProject ? 'img-fade-in' : 'img-fade-out'
-				} ${
+			  } ${
 					index === hoveredProject
 						? 'translate-x-0'
 						: 'translate-x-[-5000px]'
-				} ${index === hoveredProject ? 'opacity-100' : 'opacity-0'}`}
+			  } ${index === hoveredProject ? 'opacity-100' : 'opacity-0'}`
+			: 'w-full min-h-[250px] mt-2 rounded border border-light-100 p-4 flex flex-col justify-between gap-4';
+
+	if (width) {
+		return (
+			<div
+				className={`${
+					width >= 1200 ? 'relative' : 'flex flex-col pb-12'
+				}`}
 			>
-				<h3 className='text-2xl font-bold tracking-widest'>{title}</h3>
-				<p>{content}</p>
-				<ul className='flex flex-wrap items-center gap-2'>
-					{stack.map((stack: string, stackIndex: number) => (
-						<StackElem stack={stack} key={stackIndex} />
-					))}
-				</ul>
-				<div className='flex justify-between items-center '>
-					{links.map((link: ProjectLink, linkIndex: number) => (
-						<ProjectLinkElem link={link} key={linkIndex} />
-					))}
+				<img
+					src={img}
+					className={imgClassName}
+					alt={`${title} image`}
+				/>
+				<div className={contentWrapperClassName}>
+					<h3 className='text-2xl font-bold tracking-widest'>
+						{title}
+					</h3>
+					<p>{content}</p>
+					<ul className='flex flex-wrap items-center gap-2'>
+						{stack.map((stack: string, stackIndex: number) => (
+							<StackElem stack={stack} key={stackIndex} />
+						))}
+					</ul>
+					<div className='flex justify-between items-center '>
+						{links.map((link: ProjectLink, linkIndex: number) => (
+							<ProjectLinkElem link={link} key={linkIndex} />
+						))}
+					</div>
 				</div>
 			</div>
-		</div>
-	);
+		);
+	}
 };
 
 export default ProjectCard;
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,9 +2,7 @@ import React, { useState } from 'react';
 import useWindowDimensions from '@/hooks/useWindowDimensions';
 import ProjectList from '@/UI/ProjectList';
 import ProjectCard from '@/UI/ProjectCard';
-import { Project, ProjectLink } from '@/types';
-import StackElem from '@/UI/StackElem';
-import ProjectLinkElem from '@/UI/ProjectLinkElem';
+import { Project } from '@/types';
 import { projects } from '@/utils/utils';
 
 const Projects = () => {
@@ -43,35 +41,21 @@ const Projects = () => {
 	}
 
 	if (width && width < 1200) {
-		return projects.map((project: Project) => (
-			<div className='flex flex-col pb-12'>
-				<img
-					src={project.img}
-					className='rounded'
-					alt={`${project.title} image`}
-				/>
-				<div className='w-full min-h-[250px] mt-6 rounded border border-light-100 p-4 flex flex-col justify-between gap-4'>
-					<h3 className='text-2xl font-bold tracking-widest'>
-						{project.title}
-					</h3>
-					<p>{project.content}</p>
-					<ul className='flex flex-wrap items-center gap-2'>
-						{project.stack.map(
-							(stack: string, stackIndex: number) => (
-								<StackElem stack={stack} key={stackIndex} />
-							),
-						)}
-					</ul>
-					<div className='flex justify-between items-center '>
-						{project.links.map(
-							(link: ProjectLink, linkIndex: number) => (
-								<ProjectLinkElem link={link} key={linkIndex} />
-							),
-						)}
-					</div>
-				</div>
-			</div>
-		));
+		return (
+			<>
+				<h2 className='text-6xl font-extrabold mt-12 mb-6'>Projects</h2>
+				<ul>
+					{projects.map((project: Project, index, hoveredProject) => (
+						<ProjectCard
+							{...project}
+							key={index}
+							hoveredProject={hoveredProject}
+						/>
+					))}
+				</ul>
+				;
+			</>
+		);
 	}
 };
 
