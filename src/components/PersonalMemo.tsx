a0a3cadb0ae85f76694f2bb2cc26589c40050954
k.stopczynska <k.stopczynska@gmail.com>
2024-08-11 16:03:30 +0200

    added personal memo

--- src/components/PersonalMemo.tsx
+++ src/components/PersonalMemo.tsx
@@ -1,7 +1,39 @@
 import React from 'react';
 
 const PersonalMemo = () => {
-	return <div>PersonalMemo</div>;
+	return (
+		<article>
+			<h2 className='text-6xl font-extrabold pt-12 pb-4'>
+				About the author
+			</h2>
+			<p className='pb-4'>
+				Committed to everlasting grow, started as a self-taught
+				programmer, I expanded my skills through participation in the
+				Evolution Typescript Bootcamp, delving into Typescript, state
+				management, and 2D/3D animations. Afterwords, I completed the
+				CodeFirstGirls NanoDegree Software Stream, mastering Python,
+				Flask, and SQL. At my dayjob I'm mostly working with Java maintaining
+				and developing applications, and delving into observability, CI/CD
+				and security in the world of finance.
+			</p>
+			<p className='pb-4'>
+				Author and maintainer of codegroove- VSCode extension for
+				tracking and analyzing coding habits to master future projects'
+				estimations. Currently working on focus music module using
+				youtube API, loads of fun!
+			</p>
+			<p className='pb-4'>
+				Passionate writer about everything tech (and not only) related.
+				Passing the knowledge is what drives me to continue learning.
+			</p>
+			<p className='pb-4'>
+				In my spare time, I balance my tech passion with interests in
+				bike riding and servicing, dog training, and indulging in racing
+				games.
+			</p>
+			<p className='pb-4'>Want to connect? Go to contact section.</p>
+		</article>
+	);
 };
 
 export default PersonalMemo;
